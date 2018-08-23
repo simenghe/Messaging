fc0f4de144cac0d135607b15b12f0a3dc377bf5a
simenghe <simengschool@gmail.com>
2018-08-23 15:27:17 -0400

    Interface

--- app/src/main/java/com/example/simen/newmessages/LoginActivity.kt
+++ app/src/main/java/com/example/simen/newmessages/LoginActivity.kt
@@ -2,10 +2,23 @@ package com.example.simen.newmessages
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener{
+            performLogin()
+        }
+    }
+
+    private fun performLogin() {
+        val email=edit_login_email.text.toString()
+        val password=edit_login_password.text.toString()
+        Log.d("Login","Attempt login with email/pass $email/****")
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
     }
 }
--- app/src/main/java/com/example/simen/newmessages/MainActivity.kt
+++ app/src/main/java/com/example/simen/newmessages/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,18 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnRegister.setOnClickListener {
-            val email=editEmail.text.toString()
-            val user =editUser.text.toString()
-            val password=editPassword.text.toString()
-            Log.d("MainActivity","This is your password "+ password)
-            //Firebase first time.
-
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
-                    .addOnCompleteListener{
-                        if(!it.isSuccessful) return@addOnCompleteListener
-                        //else if successfull
-                        Log.d("Main","Success:${it.result.user.uid}")
-                    }
+            performRegister()
         }
         txtLogin.setOnClickListener{
             Log.d("MainActivity","Show login activity")
@@ -34,5 +24,27 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+    }
+
+    private fun performRegister() {
+        val email=editEmail.text.toString()
+        val user =editUser.text.toString()
+        val password=editPassword.text.toString()
+        if(email.isEmpty()||password.isEmpty()){
+            Toast.makeText(this,"Email and/or Password is empty!",Toast.LENGTH_SHORT).show()
+            return
+        }
+        Log.d("MainActivity","This is your password "+ password)
+        //Firebase first time.
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener{
+                    if(!it.isSuccessful) return@addOnCompleteListener
+                    //else if successfull
+
+                    Log.d("Main","Success:${it.result.user.uid}")
+                }
+                .addOnFailureListener{
+                    Log.d("Main","Failed to create user : ${it.message}")
+                }
     }
 }
