48672d3fee2651c267c3622b6d12b099cc1cf029
simenghe <simengschool@gmail.com>
2018-08-23 15:47:44 -0400

    Interface

--- app/src/main/java/com/example/simen/newmessages/LoginActivity.kt
+++ app/src/main/java/com/example/simen/newmessages/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.simen.newmessages
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -23,6 +24,12 @@ class LoginActivity: AppCompatActivity(){
         val password=edit_login_password.text.toString()
         Log.d("Login","Attempt login with email/pass $email/****")
         FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(){
+
+                }
+                .addOnFailureListener(){
+                    Toast.makeText(this,"Failure Occured!",Toast.LENGTH_SHORT)
+                }
     }
 
 }
